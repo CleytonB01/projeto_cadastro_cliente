7ae554b3be17de579b4cac5bb8e2f2cfb22d1d59
CleytonB01 <109830897+CleytonB01@users.noreply.github.com>
2022-08-29 16:16:28 -0300

    commit

---
+++ ProjetoFinal/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword =   '';
+    $dbName = 'camila_db';
+
+    $conexao = new mysquli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+
+?>
